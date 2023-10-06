ba5b4bd0d0d120e1c2c3bf78d61e4e083964cc41
Олег <OllegOzz@yandex.ru>
2023-10-06 19:45:22 +0300

    router auth

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import { routes } from '../router/router';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { privateRoutes, publicRoutes } from '../router/router';
 
 export default function AppRouter() {
+  const isAuth = true
+
   return (
-      <Routes>
-        {routes.map(route => 
-          <Route path={route.path} element={route.element} key={route.path}/>
-        )}
-          {/* <Route path="about" element={<About />} />
+    isAuth ?
+      <>
+        <Navigate to="/posts" />
+        <Routes>
+          {privateRoutes.map(route =>
+            <Route path={route.path}
+              element={route.element}
+              key={route.path} />
+          )}
+        </Routes>
+      </>
+      : 
+      <>
+        <Navigate to="/login" />
+        <Routes>
+          {publicRoutes.map(route =>
+            <Route path={route.path}
+              element={route.element}
+              key={route.path} />
+          )}
+        </Routes>
+      </>
+  )
+}
+
+
+
+
+
+
+{/* <Route path="about" element={<About />} />
           <Route path="posts" element={<Posts />} />
           <Route path="post/:id" element={<PostsIdPage />} />
           <Route path="/*" element={<ErrorPage />} /> */}
-      </Routes>
-
-  )
-}
