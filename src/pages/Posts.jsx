5eb656458640190f3bd64514f2e3ba272eedf106
Олег <OllegOzz@yandex.ru>
2023-10-09 16:33:46 +0300

    observer

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import '../App.css';
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
@@ -6,7 +6,6 @@ import PostFilter from '../components/PostFilter';
 import Modal from '../components/UI/Modal/Modal';
 import Button from '../components/UI/Button/Button';
 import { usePosts } from '../hooks/usePosts';
-import { useEffect } from 'react';
 import PostService from '../API/PostService';
 import Loader from '../components/UI/Loader/Loader';
 import { useFetching } from '../hooks/useFetching';
@@ -20,12 +19,16 @@ function Posts() {
     const [totalPages, setTotalPages] = useState(0)
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
+    const lastElement = useRef()
+    const observer = useRef()
 
 
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data);
+        setPosts([...posts, ...response.data]);
+        // Для постраничного вывода
+        // setPosts(response.data);         
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit))
     })
@@ -36,7 +39,19 @@ function Posts() {
     }
 
     useEffect(() => {
-        fetchPosts()
+        if (isPostsLoading) return
+        if (observer.current) observer.current.disconnect()
+        const callback = (entries, observer) => {            
+            if (entries[0].isIntersecting && page < totalPages) {
+                setPage(page + 1)
+            }
+        }
+        observer.current = new IntersectionObserver(callback);
+        observer.current.observe(lastElement.current)
+    }, [isPostsLoading])
+
+    useEffect(() => {
+        fetchPosts(limit, page)
     }, [page])
 
     const removePost = (post) => {
@@ -60,12 +75,13 @@ function Posts() {
             {postError &&
                 <h2>Произошла ошибка загрузки...<br /> {postError}</h2>
             }
-            {isPostsLoading
-                ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+            {isPostsLoading &&
+                <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
                     <Loader />
                 </div>
-                : <PostList remove={removePost} posts={sortedAndSearchPosts} title={'Список постов'} />
             }
+            <PostList remove={removePost} posts={sortedAndSearchPosts} title={'Список постов'} />
+            <div ref={lastElement} style={{ height: 10 }}></div>
             <Pagination page={page} totalPages={totalPages} setPage={setPage} />
             {/* <div className="page__wraper">
         {pageArray.map(p =>
